f9408aa1db843710bf9adedfa18056aeb9ba8793
NauvalAssidq <nauvalsidiq0427@gmail.com>
2025-04-17 21:57:27 +0700

    Referring the session_id

--- application/config/config.php
+++ application/config/config.php
@@ -383,17 +383,20 @@ $config['encryption_key'] = '';
 | except for 'cookie_prefix' and 'cookie_httponly', which are ignored here.
 |
 */
-$config['sess_driver'] = 'database';  
-$config['sess_save_path'] = 'ci_sessions'; 
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
-$config['sess_match_ip'] = FALSE;
-$config['sess_time_to_update'] = 300;  
-$config['sess_regenerate_destroy'] = FALSE;  
+$config['sess_driver']             = 'database';
+$config['sess_cookie_name']        = 'ci_session';
+$config['sess_expiration']         = 7200;
+$config['sess_save_path']          = 'ci_sessions';
+$config['sess_match_ip']           = TRUE;
+$config['sess_time_to_update']     = 300;
+$config['sess_regenerate_destroy'] = FALSE;
 
-$config['sess_cookie_httponly'] = TRUE; 
-$config['sess_secure'] = FALSE; 
-$config['sess_samesite'] = 'Lax';
+$config['sess_table_name']         = 'ci_sessions';
+$config['sess_primary_key']        = 'session_id'; // Add or modify this line
+$config['sess_ip_address']         = 'ip_address'; // Add or modify this line
+$config['sess_timestamp']          = 'last_activity'; // Add or modify this line
+$config['sess_match_useragent']    = TRUE;
+$config['sess_userdata']           = 'user_data'; // Add or modify this line
 
 
 /*
